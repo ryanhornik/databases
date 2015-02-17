a955cc4da86b878578435ebed6a63b82d3cf5528
ooplease <ryanhornik@gmail.com>
2015-02-17 15:35:35 -0600

    connected to the database and generated models

---
+++ WebApplication/SiliconShores/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SiliconShores.MvcApplication" Language="C#" %>
---
+++ WebApplication/SiliconShores/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiliconShores.Startup))]
+namespace SiliconShores
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
