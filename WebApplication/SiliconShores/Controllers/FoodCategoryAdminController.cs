9bca96b96e03b0f8be5e35d50a447f2acaa2efd6
ryanhornik <ryanhornik@gmail.com>
2015-02-28 14:32:36 -0600

    CRUD for all tables in the db. Controllers are named TableNameSingularController, so you can find them at /TableNameSingular

---
+++ WebApplication/SiliconShores/Controllers/FoodCategoryAdminController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SiliconShores.Models;
+
+namespace SiliconShores.Controllers
+{
+    public class FoodCategoryAdminController : Controller
+    {
+        private theme_park_dbEntities db = new theme_park_dbEntities();
+
+        // GET: FoodCategoryAdmin
+        public ActionResult Index()
+        {
+            return View(db.food_categories.ToList());
+        }
+
+        // GET: FoodCategoryAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            food_categories food_categories = db.food_categories.Find(id);
+            if (food_categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food_categories);
+        }
+
+        // GET: FoodCategoryAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FoodCategoryAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "food_category_id,food_categories_name")] food_categories food_categories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.food_categories.Add(food_categories);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(food_categories);
+        }
+
+        // GET: FoodCategoryAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            food_categories food_categories = db.food_categories.Find(id);
+            if (food_categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food_categories);
+        }
+
+        // POST: FoodCategoryAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "food_category_id,food_categories_name")] food_categories food_categories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(food_categories).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(food_categories);
+        }
+
+        // GET: FoodCategoryAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            food_categories food_categories = db.food_categories.Find(id);
+            if (food_categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food_categories);
+        }
+
+        // POST: FoodCategoryAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            food_categories food_categories = db.food_categories.Find(id);
+            db.food_categories.Remove(food_categories);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication/SiliconShores/Controllers/HotelReservationAdminController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SiliconShores.Models;
+
+namespace SiliconShores.Controllers
+{
+    public class HotelReservationAdminController : Controller
+    {
+        private theme_park_dbEntities db = new theme_park_dbEntities();
+
+        // GET: HotelReservationAdmin
+        public ActionResult Index()
+        {
+            var hotel_reservations = db.hotel_reservations.Include(h => h.hotel_rooms);
+            return View(hotel_reservations.ToList());
+        }
+
+        // GET: HotelReservationAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hotel_reservations hotel_reservations = db.hotel_reservations.Find(id);
+            if (hotel_reservations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_reservations);
+        }
+
+        // GET: HotelReservationAdmin/Create
+        public ActionResult Create()
+        {
+            ViewBag.hotel_id = new SelectList(db.hotel_rooms, "hotel_id", "hotel_id");
+            return View();
+        }
+
+        // POST: HotelReservationAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "reservation_id,hotel_id,room_number,reservation_checkin_date,reservation_checkout_date,total_reservation_cost,paid_in_full")] hotel_reservations hotel_reservations)
+        {
+            if (ModelState.IsValid)
+            {
+                db.hotel_reservations.Add(hotel_reservations);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.hotel_id = new SelectList(db.hotel_rooms, "hotel_id", "hotel_id", hotel_reservations.hotel_id);
+            return View(hotel_reservations);
+        }
+
+        // GET: HotelReservationAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hotel_reservations hotel_reservations = db.hotel_reservations.Find(id);
+            if (hotel_reservations == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.hotel_id = new SelectList(db.hotel_rooms, "hotel_id", "hotel_id", hotel_reservations.hotel_id);
+            return View(hotel_reservations);
+        }
+
+        // POST: HotelReservationAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "reservation_id,hotel_id,room_number,reservation_checkin_date,reservation_checkout_date,total_reservation_cost,paid_in_full")] hotel_reservations hotel_reservations)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hotel_reservations).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.hotel_id = new SelectList(db.hotel_rooms, "hotel_id", "hotel_id", hotel_reservations.hotel_id);
+            return View(hotel_reservations);
+        }
+
+        // GET: HotelReservationAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hotel_reservations hotel_reservations = db.hotel_reservations.Find(id);
+            if (hotel_reservations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_reservations);
+        }
+
+        // POST: HotelReservationAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            hotel_reservations hotel_reservations = db.hotel_reservations.Find(id);
+            db.hotel_reservations.Remove(hotel_reservations);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
