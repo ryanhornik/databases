3bf5b377c9171eb08ad1e0a5be0bf18e1bdcc7c3
ryanhornik <ryanhornik@gmail.com>
2015-03-01 16:24:04 -0600

    Connected Identity framework to mysql, Register and log in links work and write to the database
    Also removed binary files
    
    Check employee portal issue for a comment on how I think we should proceed from this point

--- WebApplication/SiliconShores/App_Start/IdentityConfig.cs
+++ WebApplication/SiliconShores/App_Start/IdentityConfig.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using AspNet.Identity.MySQL;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
@@ -40,9 +39,11 @@ namespace SiliconShores
         {
         }
 
-        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(
+                    new UserStore<ApplicationUser>(
+                    context.Get<ApplicationDbContext>() as MySQLDatabase));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
@@ -81,7 +82,7 @@ namespace SiliconShores
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
-                manager.UserTokenProvider = 
+                manager.UserTokenProvider =
                     new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
             }
             return manager;
--- WebApplication/SiliconShores/Models/IdentityModels.cs
+++ WebApplication/SiliconShores/Models/IdentityModels.cs
@@ -1,8 +1,7 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using AspNet.Identity.MySQL;
 
 namespace SiliconShores.Models
 {
@@ -18,16 +17,16 @@ namespace SiliconShores.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : MySQLDatabase
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        public ApplicationDbContext(string connectionName)
+            : base(connectionName)
         {
         }
 
         public static ApplicationDbContext Create()
         {
-            return new ApplicationDbContext();
+            return new ApplicationDbContext("IdentityConnection");
         }
     }
 }
