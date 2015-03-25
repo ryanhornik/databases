eed807ae631e8f856572d2b7843c8c505e3b09de
KopyKatt <wingblader50@gmail.com>
2015-03-24 22:09:18 -0500

    Created the model but cant figure how how to construct the view and controller from the model

---
+++ WebApplication/SiliconShores/Models/TicketsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SiliconShores.Models
+{
+    public class TicketsViewModel
+    {
+        [Display(Name = "Number of Childrens Tickets")]
+        public int ChildrenTickets { get; set; }
+
+        [Display(Name = "Number of Adult Tickets")]
+        public int AdultTickets { get; set; }
+
+        [Display(Name = "Number of Senior Tickets")]
+        public int SeniorTickets { get; set; }
+
+        [Display(Name = "Number of Military Tickets")]
+        public int MilitaryTickets { get; set; }
+    }
+}
