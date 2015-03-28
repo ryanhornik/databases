f5ee9883b98c98ee939db97d1d5eb18c3291a419
KopyKatt <wingblader50@gmail.com>
2015-03-28 12:44:27 -0500

    Figured out how to work around it, im not using a model but instead just referencing the data as i need it from the db. it could probably look better but this is just to show that it connects to the db

---
+++ WebApplication/SiliconShores/Controllers/TicketsController.cs
@@ -0,0 +1,21 @@
+﻿using SiliconShores.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace SiliconShores.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private theme_park_dbEntities db = new theme_park_dbEntities();
+
+        // GET: Tickets
+        public ActionResult TicketInformation()
+        {
+            return View(db.ticket_types.ToList());
+        }
+    }
+}
