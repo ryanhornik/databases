8e3905c8f53e2f215d4aefdaa6b0a26dd757735a
JustinHLei <lei0401@gmail.com>
2015-03-31 13:07:06 -0500

    Added ReservationCheckInModels.cs

---
+++ WebApplication/SiliconShores/Models/ReservationCheckInModels.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiliconShores.Models
+{
+    public class ReservationCheckInModels
+    {
+        [Required]
+        [Display(Name = "Arrival Date")]
+        public string arrivalDate { get; set; }
+
+        [Required]
+        [Display(Name = "Nights")]
+        public int nights { get; set; }
+
+        [Required]
+        [Display(Name = "Adults")]
+        public int adults { get; set; }
+
+        
+    }
+}
