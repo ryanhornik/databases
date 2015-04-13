97d10eaf0732261f925e66c760cc3c640889c872
ryanhornik <ryanhornik@gmail.com>
2015-04-13 00:34:46 -0500

    Got chart rendering within page - switched to ViewBag for carrying entire chart rather than a model for carrying chart data

--- WebApplication/SiliconShores/Controllers/ReportTicketSalesController.cs
+++ WebApplication/SiliconShores/Controllers/ReportTicketSalesController.cs
@@ -4,68 +4,80 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using SiliconShores.Models;
 
-  //  [Authorize]
-    public class ReportTicketSalesController: Controller
+//  [Authorize]
+public class ReportTicketSalesController : Controller
+{
+    private theme_park_dbEntities db = new theme_park_dbEntities();
+
+    // GET: report_ticketsalesAdmin
+    public ActionResult Index()
     {
-        private theme_park_dbEntities db = new theme_park_dbEntities();
-
-        // GET: report_ticketsalesAdmin
-        public ActionResult Index()
-        {
-            var ticketReport = db.report_ticketsales.Select(r => r);
-            return View(ticketReport.ToList());
-        }
-
-        // GET: report_ticketsalesAdmin/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            report_ticketsales ticketReport = db.report_ticketsales.Find(id);
-            if (ticketReport == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ticketReport);
-        }
-
-        public ActionResult DisplayTicketSalesWeather()
-        {
-            
-            var ticketReport = db.report_ticketsales.Select(r => r);
-            SortedList<string, int> chartData = new SortedList<string, int>();
-
-            foreach(var row in ticketReport)
-            {
-                if (!chartData.ContainsKey(row.weather_conditions))
-                {
-                    chartData.Add(row.weather_conditions, 1);
-                }
-                else
-                {
-                    chartData[row.weather_conditions]++;
-                }
-            }
-
-            report_ticketsalesChartData chartDataModel = new report_ticketsalesChartData();
-            chartDataModel.xValues = chartData.Keys.ToArray<String>();
-            chartDataModel.yValues = chartData.Values.ToArray<int>();
-       
-
-            return View(chartDataModel);
-        }
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        var ticketReport = db.report_ticketsales.Select(r => r);
+        return View(ticketReport.ToList());
     }
+
+    // GET: report_ticketsalesAdmin/Details/5
+    public ActionResult Details(int? id)
+    {
+        if (id == null)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        report_ticketsales ticketReport = db.report_ticketsales.Find(id);
+        if (ticketReport == null)
+        {
+            return HttpNotFound();
+        }
+        return View(ticketReport);
+    }
+
+    public ActionResult DisplayTicketSalesWeather()
+    {
+        return View();
+    }
+
+    public ActionResult GenerateChart()
+    {
+        var ticketReport = db.report_ticketsales.Select(r => r);
+        SortedList<string, int> chartData = new SortedList<string, int>();
+
+        foreach (var row in ticketReport)
+        {
+            if (!chartData.ContainsKey(row.weather_conditions))
+            {
+                chartData.Add(row.weather_conditions, 1);
+            }
+            else
+            {
+                chartData[row.weather_conditions]++;
+            }
+        }
+
+        String[] xValues = chartData.Keys.ToArray<String>();
+        int[] yValues = chartData.Values.ToArray<int>();
+
+        var myChart = new Chart(width: 400, height: 400)
+        .AddTitle("Ticket Redemption by Weather")
+        .AddSeries(
+            name: "Ticket Sales",
+            xValue: xValues,
+            yValues: yValues);
+        ViewBag.chart = myChart;
+        return View();
+
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            db.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+}
 
