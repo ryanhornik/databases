70f73a135265fde2819110c239f22e0b3c0eb8a5
ryanhornik <ryanhornik@gmail.com>
2015-04-13 17:33:54 -0500

    Switched tickets to use dictionary
    Added prices and ticket_restriction mousover to tickets and planvacation

--- WebApplication/SiliconShores/Controllers/TicketsController.cs
+++ WebApplication/SiliconShores/Controllers/TicketsController.cs
@@ -15,54 +15,27 @@ namespace SiliconShores.Controllers
          //GET: Tickets
         public ActionResult Index()
         {
-            return View(db.ticket_types.ToList());
-        }
-
-        public ActionResult Buy() 
-        {
+            ViewBag.TicketTypes = db.ticket_types.ToList();
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Buy(int childrenTickets = 0, int adultTickets = 0, int seniorTickets = 0, int militaryTickets = 0)
+        public ActionResult Index(IDictionary<int, int> ticketPurchase)
         {
-            var theme_park = db.theme_park.First(s => s.theme_park_name.Equals("Silicon Shores"));
-            var ticket_types = db.ticket_types;
-            var sale_date = DateTime.Today;
+            List<int> totalSales = new List<int>();
 
-            
-            if (ModelState.IsValid)
+            foreach (var ticket in ticketPurchase)
             {
-                ticket_sales sale = new ticket_sales();
-                sale.sale_date = sale_date;
-                sale.redemption_date = null;
-                sale.theme_park = theme_park;
-                sale.sale_location = "Online";
-
-                List<ticket_sales> fullSale = new List<ticket_sales>();
-
-                sale.ticket_types = ticket_types.First(s => s.ticket_name.Equals("Child"));
-                fullSale.AddRange(Enumerable.Repeat(sale, childrenTickets));
-
-                sale.ticket_types = ticket_types.First(s => s.ticket_name.Equals("Adult"));
-                fullSale.AddRange(Enumerable.Repeat(sale, adultTickets));
-
-                sale.ticket_types = ticket_types.First(s => s.ticket_name.Equals("Senior"));
-                fullSale.AddRange(Enumerable.Repeat(sale, seniorTickets));
-                
-                sale.ticket_types = ticket_types.First(s => s.ticket_name.Equals("Military/Veteran"));
-                fullSale.AddRange(Enumerable.Repeat(sale, militaryTickets));
-
-                foreach (var ticket in fullSale)
-                {
-                    db.ticket_sales.Add(ticket);
-                    db.SaveChanges();
-                }
-                
+                totalSales.AddRange(Enumerable.Repeat(ticket.Key, ticket.Value));
+            }
+            foreach (var s in totalSales)
+            {
+                db.ticket_sales.Add(db.CreateTicket(s));
+                db.SaveChanges();
             }
 
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", "Home");
         }
 
     }
