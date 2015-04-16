897dfaf8033a2a13d37fc04b756ff8e36171bab9
ryanhornik <ryanhornik@gmail.com>
2015-04-15 21:42:28 -0500

    Generating function for daily_ride_reports

--- WebApplication/SiliconShores/Controllers/HomeController.cs
+++ WebApplication/SiliconShores/Controllers/HomeController.cs
@@ -13,85 +13,24 @@ namespace SiliconShores.Controllers
 
         public ActionResult Index()
         {
-            var startTime = DateTime.Now;
-            string[] options = {"sunny", "partly cloudy", "mostly cloudy", "overcast", "rainy"};
-            var date = new DateTime(2014, 4, 14);
             Random r = new Random();
-            for (var k = 0; k < 365; k++)
+            var rideReports = new List<daily_ride_report>();
+            var stopDate = new DateTime(2015, 4, 14);
+            for (var date = stopDate.AddYears(-1); date < stopDate; date = date.AddDays(1))
             {
-                string weatherCondition = "";
-                var RNGeesus = r.NextDouble();
-                if (RNGeesus >= 0.0 && RNGeesus < .15)
+                int peopleInPark = db.ticket_sales.Count(s => s.redemption_date != null && (DateTime.Compare(s.redemption_date.Value ,date) == 0));
+                foreach (var ride in db.attractions)
                 {
-                    weatherCondition = options[4];
-                }
-                else if (RNGeesus < .3)
-                {
-                    weatherCondition = options[3];
-                }
-                else if (RNGeesus < .5)
-                {
-                    weatherCondition = options[2];
-                }
-                else if(RNGeesus < .7)
-                {
-                    weatherCondition = options[1];
-                }
-                else if (RNGeesus <= 1.0)
-                {
-                    weatherCondition = options[0];
-                }
-                var low_temp = r.Next(32,80);
-                var high_temp = low_temp + r.Next(1, 20);
-                daily_weather dw = new daily_weather
-                {
-                    high_temp = high_temp,
-                    low_temp = low_temp,
-                    theme_park = db.theme_park.First(s => s.theme_park_name == "Silicon Shores"),
-                    weather_conditions = weatherCondition,
-                    weather_date = date
-                };
-                db.daily_weather.Add(dw);
-                db.SaveChanges();
-
-                var numSales = 0;
-                switch (weatherCondition)
-                {
-                    case "sunny":
-                        numSales = r.Next(200);
-                        break;
-                    case "partly cloudy":
-                        numSales = r.Next(180);
-                        break;
-                    case "mostly cloudy":
-                        numSales = r.Next(125);
-                        break;
-                    case "overcast":
-                        numSales = r.Next(50);
-                        break;
-                    case "rainy":
-                        numSales = r.Next(10);
-                        break;
-                }
-                
-                for (var i = 0; i < numSales; i++)
-                {
-                    var loc = r.Next(2);
-                    
-                    var sale = new ticket_sales
+                    rideReports.Add(new daily_ride_report()
                     {
-                        sale_location = (loc == 1)?"Box Office":"Online",
-                        redemption_date = date,
-                        sale_date = (loc == 1)?date:date.AddDays(-r.Next(14)),
-                        theme_park = db.theme_park.First(s => s.theme_park_name.Equals("Silicon Shores")),
-                        ticket_types = db.ticket_types.ToArray()[r.Next(4)]
-                    };
-                    db.ticket_sales.Add(sale);
-                    db.SaveChanges();
+                    attraction = ride,
+                    ride_report_date = date,
+                    total_riders = r.Next(peopleInPark/8, (int) (peopleInPark*1.5))
+                    });
                 }
-                date = date.AddDays(1);
             }
-            ViewBag.duration = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+            db.daily_ride_report.AddRange(rideReports);
+            db.SaveChanges();
             return View();
         }
 
