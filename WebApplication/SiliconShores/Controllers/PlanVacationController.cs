a1b348030e6e202d3378f33cf55a473a75000dbb
ryanhornik <ryanhornik@gmail.com>
2015-04-23 01:57:43 -0500

    Added parameters to ConfirmPurchase GET action

--- WebApplication/SiliconShores/Controllers/PlanVacationController.cs
+++ WebApplication/SiliconShores/Controllers/PlanVacationController.cs
@@ -31,7 +31,7 @@ namespace SiliconShores.Controllers
             return View();
         }
 
-        public ActionResult ConfirmPurchase() 
+        public ActionResult ConfirmPurchase(DateTime arrivalDate, int nights, IDictionary<int, int> ticketPurchase, int Hotels, int RoomTypes, int Room) 
         {
             ViewBag.RoomTypes = db.room_types.ToList();
             ViewBag.Hotels = db.hotels.ToList();
@@ -42,7 +42,7 @@ namespace SiliconShores.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult ConfirmPurchase(DateTime arrivalDate, int nights, IDictionary<int, int> ticketPurchase, int Hotels, int RoomTypes, int Room)
+        public ActionResult ConfirmPurchase(DateTime arrivalDate, int nights, IDictionary<int, int> ticketPurchase, int Hotels, int RoomTypes, int Room, bool? post)
         {
             ticketPurchase = ticketPurchase.ToDictionary(s => s.Key, s => (nights+1)*s.Value);
 
@@ -75,67 +75,6 @@ namespace SiliconShores.Controllers
             }
 
             return RedirectToAction("Index", "Home");
-        }
-
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult CheckNow(DateTime arrivalDate, int nights, int adults, int children, int infants, room_types roomtype, int rooms)
-        {
-            var theme_park = db.theme_park.First(s => s.theme_park_name.Equals("Silicon Shores"));
-            var ticket_types = db.ticket_types;
-            
-            
-            if(ModelState.IsValid)
-            {
-                ticket_sales sale = new ticket_sales();
-                sale.sale_date = arrivalDate;
-                sale.redemption_date = null;
-                sale.theme_park = theme_park;
-                sale.sale_location = "Online";
-
-                List<ticket_sales> fullSale = new List<ticket_sales>();
-
-                if(nights > 0)
-                {
-                    nights++;
-                    adults = nights * adults;
-                    children = nights * children;
-                }
-
-                hotel_reservations reservation = new hotel_reservations();
-                reservation.reservation_checkin_date = arrivalDate;
-                TimeSpan duration = new TimeSpan(nights,0,0,0);
-                DateTime endDate = arrivalDate.Add(duration);
-                reservation.reservation_checkout_date = endDate;
-                reservation.reservation_id = 3;
-                reservation.hotel_id = 2;
-                reservation.room_number = 3;
-
-                decimal price = 102.99m;
-                reservation.total_reservation_cost = price;
-                reservation.paid_in_full = true;
-
-
-
-                sale.ticket_types = ticket_types.First(s => s.ticket_name.Equals("Adult"));
-                fullSale.AddRange(Enumerable.Repeat(sale, adults));
-
-                sale.ticket_types = ticket_types.First(s => s.ticket_name.Equals("Child"));
-                fullSale.AddRange(Enumerable.Repeat(sale, children));
-
-
-                foreach (var ticket in fullSale)
-                {
-                    db.ticket_sales.Add(ticket);
-                    db.SaveChanges();
-                }
-
-                db.hotel_reservations.Add(reservation);
-                db.SaveChanges();
-            }
-
-            return RedirectToAction("Index","Home");
         }
 
         public JsonResult RoomSelection(string selectedHotel, string selectedRoomType)
