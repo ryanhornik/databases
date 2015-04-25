8039245be55be7248bd38c1af7eea20c60bd6486
JustinHLei <lei0401@gmail.com>
2015-04-25 00:40:15 -0500

    Finished Emailing with PDF
    Barcode is working, guys.

--- WebApplication/SiliconShores/Controllers/PlanVacationController.cs
+++ WebApplication/SiliconShores/Controllers/PlanVacationController.cs
@@ -39,16 +39,9 @@ namespace SiliconShores.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult ConfirmPurchase(DateTime arrivalDate, int nights, IDictionary<int, int> ticketPurchase, int Hotels, int RoomTypes, int Room, bool? post, string email)
         {
-            GenerateBarCode();
-            GeneratePDF();
-            MailMessage mail = new MailMessage("siliconshoressmtp@gmail.com", email)
-            {
-                Subject = "PURCHASE CONFIRMATION",
-                Attachments = { new Attachment(@"C:\Users\Public\Documents\eTickets.pdf") }
-            };
-            SmtpClient client = new SmtpClient();
-            client.EnableSsl = true;
-            client.Send(mail);
+
+            List<ticket_sales> Sales = new List<ticket_sales>();
+            List<int> ticketID = new List<int>();
 
             ticketPurchase = ticketPurchase.ToDictionary(s => s.Key, s => (nights+1)*s.Value);
 
@@ -80,6 +73,24 @@ namespace SiliconShores.Controllers
                 db.SaveChanges();
             }
 
+            Sales = db.ticket_sales.OrderByDescending(t => t.ticket_id).Take(totalSales.Count()).ToList();
+            foreach (ticket_sales sales in Sales)
+            {
+                ticketID.Add(sales.ticket_id);
+            }
+
+            GenerateBarCode(ticketID);
+            GeneratePDF(ticketID.Count());
+            MailMessage mail = new MailMessage("siliconshoressmtp@gmail.com", email)
+            {
+                Subject = "PURCHASE CONFIRMATION",
+
+                Attachments = { new Attachment(@"C:\Users\Public\Documents\eTicketsForVacation.pdf") }
+            };
+            SmtpClient client = new SmtpClient();
+            client.EnableSsl = true;
+            client.Send(mail);
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -106,29 +117,32 @@ namespace SiliconShores.Controllers
                 ), JsonRequestBehavior.AllowGet);
         }
 
-        private void GenerateBarCode()
+        private void GenerateBarCode(List<int> ticketID)
         {
             BarcodeSettings setting = new BarcodeSettings();
-
-            setting.Data = "5463728191";
-            setting.Data2D = "5463728191";
-
             setting.Type = BarCodeType.UPCA;
             setting.Unit = GraphicsUnit.Millimeter;
-
             setting.X = 0.8F;
 
-            //Generate the barcode based
-            BarCodeGenerator generator = new BarCodeGenerator(setting);
+            int counter = 0;
+            foreach (int ticket_id in ticketID)
+            {
+                counter++;
+                string id = ticket_id.ToString();
+                setting.Data = id;
+                setting.Data2D = id;
 
-            Image barcode = generator.GenerateImage();
+                //Generate the barcode based
+                BarCodeGenerator generator = new BarCodeGenerator(setting);
+                Image barcode = generator.GenerateImage();
 
-            //save the barcode as an image
-            barcode.Save(@"C:\Users\Public\Documents\BarCode.png");
-
+                //save the barcode as an image
+                barcode.Save(@"C:\Users\Public\Documents\Vacation\BarCode" + counter + ".png");
+                
+            }
         }
 
-        private void GeneratePDF()
+        private void GeneratePDF(int numberOfBarcodes)
         {
             //Create a pdf document 
             PdfDocument doc = new PdfDocument();
@@ -139,43 +153,53 @@ namespace SiliconShores.Controllers
             float y = 0;
 
             //page header
-            PdfPen pen1 = new PdfPen(Color.LightGray, 1f);
-            PdfBrush brush1 = new PdfSolidBrush(Color.LightGray);
-            PdfTrueTypeFont font1 = new PdfTrueTypeFont(new Font("Arial", 8f, FontStyle.Italic));
+            PdfPen pen1 = new PdfPen(Color.Gray, 2f);
+            PdfBrush brush1 = new PdfSolidBrush(Color.Gray);
+            PdfTrueTypeFont font1 = new PdfTrueTypeFont(new Font("Arial", 10f, FontStyle.Italic));
             PdfStringFormat format1 = new PdfStringFormat(PdfTextAlignment.Right);
             String text = "Shilicon Shores Theme Park";
             page.Canvas.DrawString(text, font1, brush1, pageWidth, y, format1);
             SizeF size = font1.MeasureString(text, format1);
-            y = y + size.Height + 1;
+            y = y + size.Height + 2;
             page.Canvas.DrawLine(pen1, 0, y, pageWidth, y);
 
             //title
             y = y + 5;
             PdfBrush brush2 = new PdfSolidBrush(Color.Black);
-            PdfTrueTypeFont font2 = new PdfTrueTypeFont(new Font("Arial", 24f, FontStyle.Bold));
+            PdfTrueTypeFont font2 = new PdfTrueTypeFont(new Font("Arial", 32f, FontStyle.Bold));
             PdfStringFormat format2 = new PdfStringFormat(PdfTextAlignment.Center);
             format2.CharacterSpacing = 1f;
             text = "Thanks For Your Purchase";
             page.Canvas.DrawString(text, font2, brush2, pageWidth / 2, y, format2);
             size = font2.MeasureString(text, format2);
-            y = y + size.Height + 6;
+            y = y + size.Height + 10;
+            text = "Your eTicket:";
+            page.Canvas.DrawString(text, font2, brush2, pageWidth / 2, y, format2);
+            size = font2.MeasureString(text, format2);
+            y = y + size.Height + 10;
 
-            //icon
-            PdfImage image = PdfImage.FromFile(@"C:\Users\Public\Documents\BarCode.png");
-            page.Canvas.DrawImage(image, new PointF(pageWidth - image.PhysicalDimension.Width, y));
-            float imageLeftSpace = pageWidth - image.PhysicalDimension.Width - 2;
-            float imageBottom = image.PhysicalDimension.Height + y;
+            for (int i = 1; i <= numberOfBarcodes; i++)
+            {
+                //icon
+                PdfImage image = PdfImage.FromFile(@"C:\Users\Public\Documents\Vacation\BarCode" + i + ".png");
+                page.Canvas.DrawImage(image, new PointF(image.PhysicalDimension.Width / 2, y));
+                float imageLeftSpace = pageWidth - image.PhysicalDimension.Width - 2;
+                float imageBottom = image.PhysicalDimension.Height + y;
+                y = y + image.PhysicalDimension.Height;
+            }
 
 
             //refenrence content
+            y = y + 10;
             PdfTrueTypeFont font3 = new PdfTrueTypeFont(new Font("Arial", 9f));
             PdfStringFormat format3 = new PdfStringFormat();
             format3.ParagraphIndent = font3.Size * 2;
             format3.MeasureTrailingSpaces = true;
             format3.LineSpacing = font3.Size * 1.5f;
-            String text1 = "(All text and picture from ";
-            String text2 = "Wikipedia";
-            String text3 = ", the free encyclopedia)";
+            String text1 = "For any further questions or concerns, please contact ";
+            String text2 = "SiliconShores.net";
+            String text3 = ", ";
+            String text4 = "Customer Severice at: University of Houston  ·  4800 Calhoun Rd. Houston, TX, 77004  ·  713-743-2255";
             page.Canvas.DrawString(text1, font3, brush2, 0, y, format3);
 
             size = font3.MeasureString(text1, format3);
@@ -189,28 +213,10 @@ namespace SiliconShores.Controllers
 
             page.Canvas.DrawString(text3, font3, brush2, x1, y, format3);
             y = y + size.Height;
+            format3.ParagraphIndent = font3.Size * 2;
+            page.Canvas.DrawString(text4, font3, brush2, 0, y, format3);
 
-            //content
-            PdfStringFormat format4 = new PdfStringFormat();
-            text = "Somestuff";
-            PdfTrueTypeFont font5 = new PdfTrueTypeFont(new Font("Arial", 10f));
-            format4.LineSpacing = font5.Size * 1.5f;
-            PdfStringLayouter textLayouter = new PdfStringLayouter();
-            float imageLeftBlockHeight = imageBottom - y;
-            PdfStringLayoutResult result
-                = textLayouter.Layout(text, font5, format4, new SizeF(imageLeftSpace, imageLeftBlockHeight));
-            if (result.ActualSize.Height < imageBottom - y)
-            {
-                imageLeftBlockHeight = imageLeftBlockHeight + result.LineHeight;
-                result = textLayouter.Layout(text, font5, format4, new SizeF(imageLeftSpace, imageLeftBlockHeight));
-            }
-            foreach (LineInfo line in result.Lines)
-            {
-                page.Canvas.DrawString(line.Text, font5, brush2, 0, y, format4);
-                y = y + result.LineHeight;
-            }
-
-            doc.SaveToFile(@"C:\Users\Public\Documents\eTickets.pdf");
+            doc.SaveToFile(@"C:\Users\Public\Documents\eTicketsForVacation.pdf");
             doc.Close();
 
         }
